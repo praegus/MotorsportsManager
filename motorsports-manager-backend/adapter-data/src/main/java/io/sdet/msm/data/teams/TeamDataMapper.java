3ea9e3052cb5774c6028205e6c7d32dec3233f3e
fin.kingma <fin.kingma@ns.nl>
2024-04-22 19:23:04 +0200

    added target mapping and set to ignore

--- motorsports-manager-backend/adapter-data/src/main/java/io/sdet/msm/data/teams/TeamDataMapper.java
+++ motorsports-manager-backend/adapter-data/src/main/java/io/sdet/msm/data/teams/TeamDataMapper.java
@@ -2,11 +2,13 @@ package io.sdet.msm.data.teams;
 
 import io.sdet.msm.business.teams.Team;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TeamDataMapper {
+    @Mapping(target = "id", ignore = true)
     List<Team> map(List<TeamEntity> teams);
 
     TeamEntity map(Team team);
