180862c252beeaa6af525e49a816cc9c6a99ec8f
fin.kingma <fin.kingma@ns.nl>
2024-07-04 21:08:12 +0200

    also use fallback techniques to always show an error if something goes wrong

--- motorsports-manager-frontend/src/utils/ErrorUtil.tsx
+++ motorsports-manager-frontend/src/utils/ErrorUtil.tsx
@@ -5,6 +5,10 @@ export default class ErrorUtil {
         err.response.json().then(json => {
             if (json.errors) {
                 callback(json.errors[0].defaultMessage);
+            } else if (json.message) {
+                callback(json.message);
+            } else {
+                callback("Oeps, er is iets stuk gegaan, je moet Juriaan even bellen, dat is onze PO");
             }
         });
     }
