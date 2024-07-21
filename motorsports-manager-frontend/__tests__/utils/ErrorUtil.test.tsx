8821dd54373024e34e783bd7afffad50ba1e97f6
fin.kingma <fin.kingma@ns.nl>
2024-07-21 11:13:27 +0200

    fixed unit test

--- motorsports-manager-frontend/__tests__/utils/ErrorUtil.test.tsx
+++ motorsports-manager-frontend/__tests__/utils/ErrorUtil.test.tsx
@@ -1,31 +1,33 @@
 import { expect, test } from 'vitest'
 import {ErrorUtil} from '../../src/utils';
 
-test('ErrorUtil should return defaultMessage if available', () => {
+test('ErrorUtil should return the json from an error response', () => {
     const errorResponse = {
         response: {
             json: () => Promise.resolve({
                 detail: 'clean error message',
-                title: 'generic error message'
+                title: 'generic error message',
+                errors: [
+                    {
+                        'field': 'name',
+                        'error': 'too long'
+                    }
+                ]
             })
         }
     }
 
-    ErrorUtil.retrieveErrorMessage(errorResponse, (err) => {
-        expect(err).toBe('clean error message')
+    ErrorUtil.retrieveErrorMessage(errorResponse, (json) => {
+        expect(json.detail).toBe('clean error message')
+        expect(json.errors[0].error).toBe('too long')
     });
 });
 
-test('ErrorUtil should return generic message if no clear message is available', () => {
+test('ErrorUtil should return a clear message if nothing was received from the backend', () => {
     const errorResponse = {
-        response: {
-            json: () => Promise.resolve({
-                title: 'generic error message'
-            })
-        }
     }
 
-    ErrorUtil.retrieveErrorMessage(errorResponse, (err) => {
-        expect(err).toBe('generic error message')
+    ErrorUtil.retrieveErrorMessage(errorResponse, (json) => {
+        expect(json.detail).toBe('No response from backend.')
     });
 });
