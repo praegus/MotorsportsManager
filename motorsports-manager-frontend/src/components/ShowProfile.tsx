064c188cfc47ff1acee0ff48e4059735bb4ac631
fin.kingma <fin.kingma@ns.nl>
2024-08-12 18:27:18 +0200

    simple frontend

--- motorsports-manager-frontend/src/components/ShowProfile.tsx
+++ motorsports-manager-frontend/src/components/ShowProfile.tsx
@@ -1,11 +1,29 @@
+import { ProfileResponse, SeasonRegistrationResponse, TrackRecordResponse } from '@/generated-sources';
 import { useRouter } from 'next/router'
 
-export default function ShowProfile() {
+export default function ShowProfile({data}) {
   const router = useRouter();
+
+  function getSeason(): SeasonRegistrationResponse {
+    return data.seasonRegistrations[0];
+  }
   return (
       <div className="w-full flex">
         <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 m-auto mt-10">
             <p>Welcome! {router.query.name}</p>
+            <p>seasonName: {getSeason().name}</p>
+            <p>balance: {getSeason().accountBalance}</p>
+            <p>class: {getSeason().racingClass}</p>
+
+            <h1>Vehicle</h1>
+            <p>chassis: {getSeason().vehicle?.chassis}</p>
+            <p>engine: {getSeason().vehicle?.engine}</p>
+            <p>wheels: {getSeason().vehicle?.wheels}</p>
+
+            <h1>TrackInfo</h1>
+            {getSeason().trackRecords.length > 0 ?
+            getSeason().trackRecords?.map((record: TrackRecordResponse) => `<p>${record.name}:${record.status}:${record.prizeMoney}</p>`)
+            : `no info on current tracks!`}
         </div>
     </div>
   );
--- motorsports-manager-frontend/src/pages/profile.tsx
+++ motorsports-manager-frontend/src/pages/profile.tsx
@@ -1,11 +1,34 @@
 import ShowProfile from '../components/ShowProfile';
+import {useState, useEffect} from 'react'
 import NavBar from '../components/NavBar';
+import {ErrorResponse, ProfileApi, ProfileResponse} from '@/generated-sources';
+import { ErrorUtil } from '@/utils';
+import { useRouter } from 'next/router';
 
 export default function Profile() {
+  const [errorResponse, setErrorResponse] = useState<ErrorResponse | null>(null);
+  const [data, setData] = useState<ProfileResponse>();
+  var profileApi = new ProfileApi();
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        var profiles = await profileApi.getProfileByName({
+          name: router.query.name
+        });
+        setData(profiles);
+      } catch (errResponse: any) {
+        ErrorUtil.retrieveErrorMessage(errResponse, (json: ErrorResponse) => setErrorResponse(json))
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
       <div>
         <NavBar />
-        <ShowProfile />
+        { data? <ShowProfile data={data} /> : 'no data' }
     </div>
   );
 };
