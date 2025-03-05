2433d1721bbc167f7c5dc96d0e5e543f08d68d0a
fin.kingma <fin.kingma@ns.nl>
2025-03-05 13:53:37 +0100

    added cucumber json report

--- motorsports-manager-backend/configuration/src/test/java/io/sdet/msm/configuration/CucumberRunnerTest.java
+++ motorsports-manager-backend/configuration/src/test/java/io/sdet/msm/configuration/CucumberRunnerTest.java
@@ -2,14 +2,18 @@ package io.sdet.msm.configuration;
 
 import io.cucumber.java.Before;
 import io.sdet.msm.data.profile.ProfileRepositoryJPA;
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber.json")
 public class CucumberRunnerTest {
 
     @Autowired
